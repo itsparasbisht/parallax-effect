dae7a579d9a5b625bc184d856862a3bb77b92c5e
itsparasbisht <iamparasbisht@gmail.com>
2023-12-04 13:54:56 +0530

    intersection observer for optimized scroll effect

--- script.js
+++ script.js
@@ -3,15 +3,44 @@ let leaf = document.getElementById("leaf");
 let hill1 = document.getElementById("hill1");
 let hill4 = document.getElementById("hill4");
 let hill5 = document.getElementById("hill5");
+let parallaxContainer = document.getElementById("parallaxContainer");
+let isParallaxContainerIntersecting = true;
+let sectionEnd = document.getElementById("sec-end");
+
+const parallaxContainerObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        isParallaxContainerIntersecting = true;
+      } else {
+        isParallaxContainerIntersecting = false;
+      }
+    });
+  },
+  { threshold: 0 }
+);
+parallaxContainerObserver.observe(parallaxContainer);
+
+const sectionContainerObserver = new IntersectionObserver((entries) => {
+  const endingSpan = entries[0];
+  if (endingSpan.isIntersecting) {
+    isParallaxContainerIntersecting = false;
+    parallaxContainerObserver.unobserve(parallaxContainer);
+  } else {
+    isParallaxContainerIntersecting = true;
+    parallaxContainerObserver.observe(parallaxContainer);
+  }
+});
+sectionContainerObserver.observe(sectionEnd);
 
 window.addEventListener("scroll", () => {
-  let value = window.scrollY;
-  console.log(value);
-
-  text.style.marginTop = value * 2.5 + "px";
-  leaf.style.top = value * -1.5 + "px";
-  leaf.style.left = value * 1.5 + "px";
-  hill5.style.left = value * 1.5 + "px";
-  hill4.style.left = value * -1.5 + "px";
-  hill1.style.top = value * 1 + "px";
+  if (isParallaxContainerIntersecting) {
+    let value = window.scrollY;
+    text.style.marginTop = value * 2.5 + "px";
+    leaf.style.top = value * -1.5 + "px";
+    leaf.style.left = value * 1.5 + "px";
+    hill5.style.left = value * 1.5 + "px";
+    hill4.style.left = value * -1.5 + "px";
+    hill1.style.top = value * 1 + "px";
+  }
 });
